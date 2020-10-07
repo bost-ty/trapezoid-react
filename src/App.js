2ffe22f7a60448f680d2fb03efb040a220a1e4a9
bost-ty <61333914+bost-ty@users.noreply.github.com>
2020-10-06 20:48:35 -0700

    Fix spelling error(s) in About section

--- src/App.js
+++ src/App.js
@@ -44,10 +44,10 @@ function About() {
         Created with <code>npx create-react-app</code>. To run locally, clone this repository, <code>cd</code> into the directory, and run <code>npm start</code> for a local development server. 
       </p>
     <p>
-      This project is a continuation of a Python3 program <cpde>trapezoid.py</code> that was converted into a vanilla JavaScript file <code>trapezoid.js</code>, and has now been converted again into a much nicer and more useful interface as `trapezoid-react`.
+      This project is a continuation of a Python3 program <code>trapezoid.py</code> that was converted into a vanilla JavaScript file <code>trapezoid.js</code>, and has now been converted again into a much nicer and more useful interface as <code>trapezoid-react</code>.
     </p>
     <p>
-      This is probably not a great use case for React, and I understnad that.
+      This is probably not a great use case for React; I regret nothing.
     </p>
     </article>
   );
