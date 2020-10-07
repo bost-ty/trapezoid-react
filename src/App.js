6c61a9363842ce414b8ea9aace3000e01864087d
Will Bostwick <wjrbostwick@gmail.com>
2020-10-06 17:45:17 -0700

    Add more accessible styles, placeholder onChange

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./assets/normalize.css";
 import "./App.css";
 
-import { FaGithub, FaQuestionCircle } from "react-icons/fa";
+import { FaCalculator, FaGithub, FaQuestionCircle } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Calculator />
+      <About />
     </div>
   );
 }
@@ -24,12 +26,62 @@ function Header() {
           </a>
         </li>
         <li>
-          <a href="https://github.com/bost-ty/trapezoid-react/ABOUT.md">
+          <a href="#about">
             <FaQuestionCircle />
           </a>
         </li>
       </ul>
     </header>
+  );
+}
+
+function Calculator() {
+  const [longBase, setLongBase] = useState(0);
+  const [shortBase, setShortBase] = useState(0);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} onChange={handleSubmit}>
+        <label>
+          Long base:
+          <input
+            type="text"
+            name="longBase"
+            value={longBase}
+            onChange={(e) => setLongBase(e.target.value)}
+          />
+        </label>
+        <label>
+          Short base:
+          <input
+            type="text"
+            name="shortBase"
+            value={shortBase}
+            onChange={(e) => setShortBase(e.target.value)}
+          />
+        </label>
+        <button type="submit" name="submit">
+          <FaCalculator className="fa-calc" />
+        </button>
+      </form>
+    </div>
+  );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleFormChange(e);
+  }
+
+  function handleFormChange(e) {
+    console.log("Form change" + " " + e.target.name);
+  }
+}
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
   );
 }
 
