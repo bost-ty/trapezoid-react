01852fee78209b07f1a1155e41d2b592d3bfcbd1
Will Bostwick <wjrbostwick@gmail.com>
2020-10-07 10:23:03 -0700

    Update tagline, color About section

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # trapezoid-react 🍟
 
->Bootstrapped with `create-react-app`. To run locally, clone this repository, `cd` into the root directory, and run `npm start`.
+A small app for calculating neat trapezoids.
+
+> Bootstrapped with `create-react-app`. To run locally, clone this repository, `cd` into the directory, and run `npm install` and then `npm start`.
 
 ### About `trapezoid-react`
 
 This project is a continuation of a Python3 program, `trapezoid.py`, that was converted into a vanilla JavaScript file, `trapezoid.js`, and now into a much nicer and more useful interface as `trapezoid-react`. See <https://github.com/bost-ty/trapezoid> for more information on the origins of this project.
 
+No, this isn't a great use case for React!
--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function Header() {
   return (
     <header>
       <h1>trapezoid.jsx</h1>
-      <p>a small app for calculating trapezoids</p>
+      <p>a small app for calculating neat trapezoids</p>
       <ul>
         <li>
           <a href="https://github.com/bost-ty/trapezoid-react">
@@ -41,14 +41,17 @@ function About() {
     <article id="about">
       <h2>About</h2>
       <p>
-        Created with <code>npx create-react-app</code>. To run locally, clone this repository, <code>cd</code> into the directory, and run <code>npm start</code> for a local development server. 
+        Created with <code>npx create-react-app</code>. To run locally, clone
+        this repository, <code>cd</code> into the directory, and run{" "}
+        <code>npm start</code> for a local development server.
       </p>
-    <p>
-      This project is a continuation of a Python3 program <code>trapezoid.py</code> that was converted into a vanilla JavaScript file <code>trapezoid.js</code>, and has now been converted again into a much nicer and more useful interface as <code>trapezoid-react</code>.
-    </p>
-    <p>
-      This is probably not a great use case for React; I regret nothing.
-    </p>
+      <p>
+        This project is a continuation of a Python3 program{" "}
+        <code>trapezoid.py</code> that was converted into a vanilla JavaScript
+        file <code>trapezoid.js</code>, and has now been converted again into a
+        much nicer and more useful interface as <code>trapezoid-react</code>.
+      </p>
+      <p>No, this isn't a great use case for React!</p>
     </article>
   );
 }
