1e41656501ae11d4caf266410ba87b697076f501
Will Bostwick <wjrbostwick@gmail.com>
2020-10-07 17:06:01 -0700

    Fighting z-indexes and transitions

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,18 @@
 import React from "react";
 
 export default function About({ showAbout }) {
-  return (
-    <article
-      id="about"
-      className={showAbout ? null : "hidden"}
-      aria-hidden={!showAbout}
-    >
-      <h2>About</h2>
-      <p>
-        This project is a continuation of a Python program written for a
-        woodworking project. It has been converted to{" "}
-        <a href="https://github.com/bost-ty/trapezoid">vanilla JavaScript</a>{" "}
-        already. This is the newest version, <code>trapezoid-react</code>.
-      </p>
-    </article>
-  );
+	return (
+		<>
+			<div id="overlay" aria-hidden="true"></div>
+			<article id="about" className={showAbout ? null : "hidden"} aria-hidden={!showAbout}>
+				<h2>About</h2>
+				<p>
+					This project is a continuation of a Python program written for a woodworking project. It
+					has been converted to{" "}
+					<a href="https://github.com/bost-ty/trapezoid">vanilla JavaScript</a> already. This is the
+					newest version, <code>trapezoid-react</code>.
+				</p>
+			</article>
+		</>
+	);
 }
